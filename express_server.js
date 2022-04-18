15b1dd74b3f8209808cd0c495b7ec9f49ffe293c
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-18 21:07:31 +0000

    added urls_new to create new shortURLs

--- express_server.js
+++ express_server.js
@@ -2,7 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
+
+function generateRandomString() {
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    let code = Math.floor(Math.random() * 36);
+    if (code < 10) {
+      result += code.toString();
+    } else {
+      result += String.fromCharCode(code + 87);
+    }
+  }
+  return result;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,6 +33,15 @@ app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
 }); 
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+});
 
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
